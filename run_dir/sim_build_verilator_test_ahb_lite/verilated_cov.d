bfc54259f20016c3b57ff72d8ea0ba6c0879fa8a
Anderson Ignacio <anderson@aignacio.com>
2023-10-01 16:21:37 +0100

    Removing run dir
    Signed-off-by: Anderson Ignacio <anderson@aignacio.com>

--- run_dir/sim_build_verilator_test_ahb_lite/verilated_cov.d
+++
@@ -1,8 +0,0 @@
-verilated_cov.o: /usr/local/share/verilator/include/verilated_cov.cpp \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_cov.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- /usr/local/share/verilator/include/verilated_cov_key.h
