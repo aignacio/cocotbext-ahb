244911171a8642555c02fda366734fa8a714ef32
Anderson Ignacio <anderson@aignacio.com>
2023-10-25 21:59:31 +0100

    Trying to include coverage
    Signed-off-by: Anderson Ignacio <anderson@aignacio.com>

--- noxfile.py
+++ noxfile.py
@@ -4,29 +4,42 @@
 # License           : MIT license <Check LICENSE>
 # Author            : Anderson I. da Silva (aignacio) <anderson@aignacio.com>
 # Date              : 08.10.2023
-# Last Modified Date: 24.10.2023
+# Last Modified Date: 25.10.2023
 
 import nox
 
 
 @nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def run(session):
-    session.env['DUT'] = 'ahb_template'
-    session.env['SIM'] = 'icarus'
+    session.env["DUT"] = "ahb_template"
+    session.env["SIM"] = "icarus"
     # session.env['SIM'] = 'verilator'
-    session.env['TIMEPREC'] = '1ps'
-    session.env['TIMEUNIT'] = '1ns'
-    session.install('pytest',
-                    'pytest-xdist',
-                    'pytest-split',
-                    'cocotb-bus == 0.2.1',
-                    'cocotb-test == 0.2.4',
-                    'cocotb >= 1.8.0')
+    session.env["TIMEPREC"] = "1ps"
+    session.env["TIMEUNIT"] = "1ns"
+    session.install(
+        "pytest",
+        "pytest-xdist",
+        "pytest-cov",
+        "pytest-split",
+        "cocotb-bus == 0.2.1",
+        "cocotb-test == 0.2.4",
+        "cocotb >= 1.8.0",
+    )
     session.install("-e", ".")
-    session.run('pytest', '-rP', '-n', 'auto', *session.posargs)
+    session.run(
+        "pytest",
+        "--cov=cocotbext",
+        "--cov=tests",
+        "--cov-branch",
+        "--cov-report=xml",
+        "-rP",
+        "-n",
+        "auto",
+        *session.posargs
+    )
 
 
 @nox.session(python=["3.9", "3.10"])
 def lint(session):
-    session.install('flake8')
-    session.run('flake8')
+    session.install("flake8")
+    session.run("flake8")
