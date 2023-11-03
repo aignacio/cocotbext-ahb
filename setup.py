0008492f8fc9fcad968ca323668f919328fa54c3
Anderson Ignacio <anderson@aignacio.com>
2023-11-03 17:25:36 +0000

    Updated setup.py to include namespace find
    Signed-off-by: Anderson Ignacio <anderson@aignacio.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 import codecs
 import os
 from cocotbext.ahb.version import __version__
 
-DESCRIPTION = 'CocotbExt AHB Bus VIP'
-LONG_DESCRIPTION = 'AHB VIP for Bus interface'
+DESCRIPTION = "CocotbExt AHB Bus VIP"
+LONG_DESCRIPTION = "AHB VIP for Bus interface"
 
 here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
@@ -13,24 +13,29 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 # Setting up
 setup(
     name="cocotbext-ahb",
+    packages=find_namespace_packages(include=["cocotbext.*"]),
     version=__version__,
     author="aignacio (Anderson Ignacio)",
     author_email="<anderson@aignacio.com>",
+    license="MIT",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
-    url='https://github.com/aignacio/cocotbext-ahb',
-    packages=find_packages(
-        include="cocotbext.*"
-    ),
+    url="https://github.com/aignacio/cocotbext-ahb",
+    project_urls={
+        "Bug Tracker": "https://github.com/aignacio/cocotbext-ahb/issues",
+        "Source Code": "https://github.com/aignacio/cocotbext-ahb",
+    },
     include_package_data=False,
-    python_requires='>=3.6',
-    install_requires=[
-        'cocotb>=1.8.0',
-        'cocotb-bus>=0.2.1',
-        'cocotb-test>=0.2.4'
-    ],
-    keywords=['soc', 'vip', 'hdl', 'verilog', 'systemverilog', 'ahb'],
+    python_requires=">=3.6",
+    install_requires=["cocotb>=1.8.0", "cocotb-bus>=0.2.1", "cocotb-test>=0.2.4"],
+    extras_require={
+        "test": [
+            "pytest",
+            "cocotb-test",
+        ],
+    },
+    keywords=["soc", "vip", "hdl", "verilog", "systemverilog", "ahb"],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
@@ -39,5 +44,6 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+    ],
 )
